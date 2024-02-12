0b2b90f1fe63131ee2ae185a29179f96c11b3bbc
DevExpressExampleBot <fakeEmail>
2024-02-12 19:09:36 +0400

    Source auto update [skip ci]

--- VB/TBExample/MainWindow.xaml.vb
+++ VB/TBExample/MainWindow.xaml.vb
@@ -1,100 +1,102 @@
-﻿Imports DevExpress.Mvvm
+Imports DevExpress.Mvvm
 Imports System.Collections.ObjectModel
 Imports System.Windows
 
 Namespace TBExample
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
-	End Class
-	Public Class ViewModel
-		Public Sub New()
-			Items = New ObservableCollection(Of Item)()
-			Items.Add(New Item() With {.Name = "Home"})
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-			Dim item1 As New Item() With {
-				.Name = "Item1",
-				.Group = "Group1"
-			}
-			item1.Children = New ObservableCollection(Of Item)()
-			item1.Children.Add(New Item() With {.Name = "Item1Child1"})
-			item1.Children.Add(New Item() With {.Name = "Item1Child2"})
-			Items.Add(item1)
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
 
-			item1 = New Item() With {
-				.Name = "Item2",
-				.Group = "Group2"
-			}
-			item1.Children = New ObservableCollection(Of Item)()
-			item1.Children.Add(New Item() With {.Name = "Item2Child1"})
-			Items.Add(item1)
+    Public Class ViewModel
 
-		End Sub
-		Public Property Items() As ObservableCollection(Of Item)
-	End Class
+        Public Sub New()
+            Items = New ObservableCollection(Of Item)()
+            Items.Add(New Item() With {.Name = "Home"})
+            Dim item1 As Item = New Item() With {.Name = "Item1", .Group = "Group1"}
+            item1.Children = New ObservableCollection(Of Item)()
+            item1.Children.Add(New Item() With {.Name = "Item1Child1"})
+            item1.Children.Add(New Item() With {.Name = "Item1Child2"})
+            Items.Add(item1)
+            item1 = New Item() With {.Name = "Item2", .Group = "Group2"}
+            item1.Children = New ObservableCollection(Of Item)()
+            item1.Children.Add(New Item() With {.Name = "Item2Child1"})
+            Items.Add(item1)
+        End Sub
 
-	Public Class Item
-		Inherits BindableBase
+        Public Property Items As ObservableCollection(Of Item)
+    End Class
 
-		Public Sub New()
-			Command = New DelegateCommand(AddressOf CommandAction)
-		End Sub
+    Public Class Item
+        Inherits BindableBase
 
-		Protected _Name As String
-		Public Property Name() As String
-			Get
-				Return Me._Name
-			End Get
-			Set(ByVal value As String)
-				Me.SetProperty(Me._Name, value, "Name")
-			End Set
-		End Property
+        Public Sub New()
+            Command = New DelegateCommand(AddressOf CommandAction)
+        End Sub
 
-		Protected _Group As String
-		Public Property Group() As String
-			Get
-				Return Me._Group
-			End Get
-			Set(ByVal value As String)
-				Me.SetProperty(Me._Group, value, "Group")
-			End Set
-		End Property
+        Protected _Name As String
 
+        Public Property Name As String
+            Get
+                Return _Name
+            End Get
 
-		Protected _Children As ObservableCollection(Of Item)
-		Public Property Children() As ObservableCollection(Of Item)
-			Get
-				Return Me._Children
-			End Get
-			Set(ByVal value As ObservableCollection(Of Item))
-				Me.SetProperty(Me._Children, value, "Children")
-			End Set
-		End Property
-		Public ReadOnly Property IsHasChildren() As Boolean
-			Get
-				Return Children IsNot Nothing AndAlso Children.Count <> 0
-			End Get
-		End Property
+            Set(ByVal value As String)
+                SetProperty(_Name, value, "Name")
+            End Set
+        End Property
 
+        Protected _Group As String
 
-		Protected _Command As DelegateCommand
-		Public Property Command() As DelegateCommand
-			Get
-				Return Me._Command
-			End Get
-			Set(ByVal value As DelegateCommand)
-				Me.SetProperty(Me._Command, value, "Command")
-			End Set
-		End Property
+        Public Property Group As String
+            Get
+                Return _Group
+            End Get
 
-		Public Sub CommandAction()
-			MessageBox.Show(Name)
-		End Sub
-	End Class
+            Set(ByVal value As String)
+                SetProperty(_Group, value, "Group")
+            End Set
+        End Property
+
+        Protected _Children As ObservableCollection(Of Item)
+
+        Public Property Children As ObservableCollection(Of Item)
+            Get
+                Return _Children
+            End Get
+
+            Set(ByVal value As ObservableCollection(Of Item))
+                SetProperty(_Children, value, "Children")
+            End Set
+        End Property
+
+        Public ReadOnly Property IsHasChildren As Boolean
+            Get
+                Return Children IsNot Nothing AndAlso Children.Count <> 0
+            End Get
+        End Property
+
+        Protected _Command As DelegateCommand
+
+        Public Property Command As DelegateCommand
+            Get
+                Return _Command
+            End Get
+
+            Set(ByVal value As DelegateCommand)
+                SetProperty(_Command, value, "Command")
+            End Set
+        End Property
+
+        Public Sub CommandAction()
+            MessageBox.Show(Name)
+        End Sub
+    End Class
 End Namespace
